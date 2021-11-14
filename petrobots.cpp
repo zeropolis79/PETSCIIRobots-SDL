f9785754500c69883ee014e27afeadf50e5e9c0a
Vesa Halttunen <vesuri@jormas.com>
2021-11-14 20:25:09 +0100

    Define only one function in Platform related to rendering the live map

--- petrobots.cpp
+++ petrobots.cpp
@@ -1455,8 +1455,7 @@ void DRAW_LIVE_MAP()
         LIVE_MAP_ROBOTS_BLINK++;
         LIVE_MAP_ROBOTS_BLINK &= 31;
     }
-    platform->renderLiveMap(MAP);
-    platform->renderLiveMapUnits(UNIT_TYPE, UNIT_LOC_X, UNIT_LOC_Y, (LIVE_MAP_ROBOTS_ON == 1 && LIVE_MAP_ROBOTS_BLINK < 16) ? true : false);
+    platform->renderLiveMap(MAP, UNIT_TYPE, UNIT_LOC_X, UNIT_LOC_Y, (LIVE_MAP_ROBOTS_ON == 1 && LIVE_MAP_ROBOTS_BLINK < 16) ? true : false);
 }
 
 uint8_t LIVE_MAP_ON = 0;
