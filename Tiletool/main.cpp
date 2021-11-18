1c8b4129a26b1a509e826d68aae54122739733c3
Vesa Halttunen <vesuri@jormas.com>
2021-11-18 19:25:03 +0100

    Add reverse option

--- Tiletool/main.cpp
+++ Tiletool/main.cpp
@@ -22,9 +22,13 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; i++) {
         args << argv[i];
     }
+    QCommandLineOption reverseOption(QStringList() << "r" << "reverse", QCoreApplication::translate("main", "Convert back to 16x16."));
+    parser.addOption(reverseOption);
     parser.addPositionalArgument("input", QCoreApplication::translate("main", "The name of the image to be converted."));
     parser.addPositionalArgument("output", QCoreApplication::translate("main", "The basename of the converted image."));
     parser.process(args);
+
+    bool reverse = parser.isSet(reverseOption);
 
     const QStringList positionalArguments = parser.positionalArguments();
 
@@ -43,23 +47,41 @@ int main(int argc, char *argv[])
     standardError << QCoreApplication::translate("main", "Size ") << inputImage.width() << "x" << inputImage.height() << "\n";
     standardError << QCoreApplication::translate("main", "Colors ") << inputImage.colorCount() << "\n";
 
-    QImage outputImage(32, 24 * 256, inputImage.format());
-    outputImage.setColorCount(inputImage.colorCount());
-    outputImage.setColorTable(inputImage.colorTable());
-    for (int tile = 0; tile < 256; tile++) {
-        int inputX = (tile % 16) * 24;
-        int inputY = (tile / 16) * 24;
-        int outputY = tile * 24;
-        for (int y = 0; y < 24; y++) {
-            for (int x = 0; x < 24; x++) {
-                outputImage.setPixel(x, outputY + y, inputImage.pixelIndex(inputX + x, inputY + y));
-            }
-            for (int x = 24; x < 32; x++) {
-                outputImage.setPixel(x, outputY + y, 0);
+    if (reverse) {
+        QImage outputImage(24 * 16, 24 * 16, inputImage.format());
+        outputImage.setColorCount(inputImage.colorCount());
+        outputImage.setColorTable(inputImage.colorTable());
+        for (int tile = 0; tile < 256; tile++) {
+            int inputY = tile * 24;
+            int outputX = (tile % 16) * 24;
+            int outputY = (tile / 16) * 24;
+            for (int y = 0; y < 24; y++) {
+                for (int x = 0; x < 24; x++) {
+                    outputImage.setPixel(outputX + x, outputY + y, inputImage.pixelIndex(x, inputY + y));
+                }
             }
         }
+        outputImage.save(positionalArguments.at(1));
+
+    } else {
+        QImage outputImage(32, 24 * 256, inputImage.format());
+        outputImage.setColorCount(inputImage.colorCount());
+        outputImage.setColorTable(inputImage.colorTable());
+        for (int tile = 0; tile < 256; tile++) {
+            int inputX = (tile % 16) * 24;
+            int inputY = (tile / 16) * 24;
+            int outputY = tile * 24;
+            for (int y = 0; y < 24; y++) {
+                for (int x = 0; x < 24; x++) {
+                    outputImage.setPixel(x, outputY + y, inputImage.pixelIndex(inputX + x, inputY + y));
+                }
+                for (int x = 24; x < 32; x++) {
+                    outputImage.setPixel(x, outputY + y, 0);
+                }
+            }
+        }
+        outputImage.save(positionalArguments.at(1));
     }
-    outputImage.save(positionalArguments.at(1));
 
     return 0;
 }
