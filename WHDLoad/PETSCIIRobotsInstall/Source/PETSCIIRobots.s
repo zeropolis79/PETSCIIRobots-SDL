a9e803267a8b76c072358becb4078eb44f57aa47
Vesa Halttunen <vesuri@jormas.com>
2021-12-22 23:30:41 +0100

    Clean up the WHDLoad slave, adding CRC16 checking

--- WHDLoad/PETSCIIRobotsInstall/Source/PETSCIIRobots.s
+++ WHDLoad/PETSCIIRobotsInstall/Source/PETSCIIRobots.s
@@ -31,13 +31,13 @@ slv_keyexit	= $59	; F10
 
 	INCLUDE	"kick13.s"
 
-	dc.b	"$VER: PETSCIIRobots.slave 1.0 (21.12.2021)",0
+	dc.b	"$VER: PETSCIIRobots.slave 1.0 (22.12.2021)",0
 
 slv_CurrentDir:	dc.b	"Data",0
 slv_name:		dc.b	"Attack of the PETSCII Robots",0
 slv_copy:		dc.b	"2021 8-Bit Productions, LLC.",0
 slv_info:		dc.b	"Adapted by Vesa Halttunen",10
-				dc.b	"Version 1.0 (21.12.2021)",0
+				dc.b	"Version 1.0 (22.12.2021)",0
 programName:	dc.b	"AmigaRobots",0
 args:			dc.b	10
 argsEnd:		dc.b	0
@@ -50,40 +50,12 @@ _bootdos:	move.l	_resload,a2
 	jsr	_LVOOldOpenLibrary(a6)
 	move.l	d0,a6
 
-	lea	programName(pc),a0
-	move.l	a0,d1
+	lea	programName(pc),a3
+	move.l	a3,d1
 	jsr	_LVOLoadSeg(a6)
-	move.l	d0,d7
-	beq	.quit
+	tst.l	d0
+	bne.s	.checkVersion
 
-	move.l	d7,a1
-	add.l	a1,a1
-	add.l	a1,a1
-	addq.l	#4,a1
-
-	cmp.w	#$4e92,$2a68(a1)	; jsr(a2)
-	beq	.patch
-	pea	TDREASON_WRONGVER
-	jmp	resload_Abort(a2)
-
-.patch:
-	movem.l	a1,-(sp)
-	lea	patchList(pc),a0	
-	jsr	resload_Patch(a2)
-	move.l	(sp)+,a1
-
-	lea	args(pc),a0
-	move.l	4(sp),d0
-	sub.l	#5*4,d0
-	movem.l	d0/d7/a2/a6,-(sp)
-	moveq	#argsEnd-args,d0
-	jsr	(a1)
-	movem.l	(sp)+,d1/d7/a2/a6
-
-	pea	TDREASON_OK
-	jmp	resload_Abort(a2)
-
-.quit:
 	jsr	_LVOIoErr(a6)
 	move.l	a3,-(sp)
 	move.l	d0,-(sp)
@@ -91,6 +63,36 @@ _bootdos:	move.l	_resload,a2
 	move.l	_resload(pc),-(sp)
 	add.l	#resload_Abort,(sp)
 	rts
+
+.checkVersion:
+	add.l	d0,d0
+	add.l	d0,d0
+	addq	#4,d0
+	move.l	d0,a3
+
+	move.l	a3,a0
+	move.l	#$907c,d0
+	jsr	resload_CRC16(a2)
+	move.w	d0,$41000000
+	cmp.w	#$683a,d0
+	beq	.patch
+	pea	TDREASON_WRONGVER
+	jmp	resload_Abort(a2)
+
+.patch:
+	lea	patchList(pc),a0	
+	move.l	a3,a1
+	jsr	resload_Patch(a2)
+
+	lea	args(pc),a0
+	move.l	4(sp),d0
+	sub.l	#5*4,d0
+	moveq	#argsEnd-args,d0
+	jsr	(a3)
+
+	move.l	_resload,a2
+	pea	TDREASON_OK
+	jmp	resload_Abort(a2)
 
 patchList:
 	PL_START
