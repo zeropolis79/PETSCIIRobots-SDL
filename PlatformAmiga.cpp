9abec9dac8d564fc9996fec0dd7daaf5f3907df1
Vesa Halttunen <vesuri@jormas.com>
2022-05-31 12:50:26 +0200

    Bump version

--- PlatformAmiga.cpp
+++ PlatformAmiga.cpp
@@ -62,9 +62,9 @@
 #define PRELOADED_ASSETS_BUFFER_SIZE_ALL (PRELOADED_ASSETS_BUFFER_SIZE_MINIMUM + 71432 + 103754 + 105654 + 86504 + 14 * 8960)
 #endif
 #if PLATFORM_MAP_COUNT > 2
-static const char version[] = "$VER: Attack of the PETSCII Robots 1.2 (2022-05-16)";
+static const char version[] = "$VER: Attack of the PETSCII Robots 1.2 (2022-05-31)";
 #else
-static const char version[] = "$VER: Attack of the PETSCII Robots Shareware (2022-05-16)";
+static const char version[] = "$VER: Attack of the PETSCII Robots Shareware (2022-05-31)";
 #endif
 
 struct SpriteData {
